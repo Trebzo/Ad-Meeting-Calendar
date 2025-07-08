ce62f26f5327bba54a66c0b0137f9ffe14667e1c
Ozbert <202310393@fit.edu.ph>
2025-07-08 20:05:02 +0800

    feat(paths): add new path definitions for dummies, layouts, components, pages, and assets in bootstrap.php chore(composer): update PostgreSQL scripts to use relative paths in composer.json

--- bootstrap.php
+++ bootstrap.php
@@ -3,4 +3,10 @@ define('BASE_PATH', realpath(__DIR__));
 define('UTILS_PATH', BASE_PATH . '/utils/');
 define('VENDOR_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
+define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
+define('LAYOUTS_PATH', BASE_PATH . '/layouts/');
+define('COMPONENTS_PATH', BASE_PATH . '/components/');
+define('PAGES_PATH', BASE_PATH . '/pages/');
+define('ASSETS_PATH', BASE_PATH . '/assets/');
+
 chdir(BASE_PATH);
