f8974d9d10b58f0cde39d64a84e0a00daa0888ab
Ozbert <202310393@fit.edu.ph>
2025-07-08 20:02:30 +0800

    fix(envSetter): correct path to autoload.php in environment setter utility

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -3,7 +3,7 @@
 if (!defined('BASE_PATH')) {
     define('BASE_PATH', realpath(dirname(__DIR__)));
 }
-require_once BASE_PATH . '/vendor/autoload.php';
+require_once VENDOR_PATH . 'autoload.php';
 
 
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
